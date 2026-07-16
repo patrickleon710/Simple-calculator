c0abdf32a3c0b9708d5cf071d19c22a9fea09c50
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2026-07-16 14:48:14 +0300

    Modified custom function to work with multi-digit numbers using .split()

--- ui.py
+++ ui.py
@@ -39,17 +39,20 @@ class Calculator:
         signFrame = tk.Frame(root, bg="purple")
         signFrame.pack(side="right", fill="both", expand="true", padx=10, pady=10)
 
-        signs = ["/", "x", "-", "+", "="]
+        signs = [" / ", " x ", " - ", " + ", "<", "C", "="]
         for r in range(4):
             signFrame.rowconfigure(r, weight=1)
 
         for i, text in enumerate(signs):
-            btn = tk.Button(signFrame, text=text, command=self.calculate_expression if text == "="  else lambda t=text: self.createText(t))
+            
+            btn = tk.Button(signFrame, text=text, command=cmd)
             btn.grid(row=i, column=0, sticky="nsew", padx=2, pady=2)
         
     def createText(self, text):            
             self.input.insert(tk.END,text)
-  
+    
+   
+    
 ## Using python's built-in eval function 
     def evaluate_input(self):
         text = self.input.get()
@@ -64,11 +67,53 @@ class Calculator:
         self.input.delete(0,tk.END)
         self.input.insert(tk.END,result)
         
-## Using custom function
+## Using custom function - works if there are spaces between numbers and spaces
     def calculate_expression(self):
         
         text = self.input.get()
+        cleaned_text = text.replace("x ", "* ")
+        tokens = list(cleaned_text) # with no spaces
+        expr_tok = cleaned_text.split() # with spaces
+        i = 0
+        if not expr_tok:
+            return
+        print(expr_tok)
+        while i < len(expr_tok):
+            if expr_tok[i] in ["*", "/"]:
+                num1 = float(expr_tok[i-1])
+                operator = expr_tok[i]
+                num2 = float(expr_tok[i+1])
+                if operator == "/":
+                    subresult = num1 / num2
+                elif operator == "*":
+                    subresult = num1 * num2
+                expr_tok[i-1 : i+2] = [str(subresult)]
+                i-=1            
+            i+=1
+        result = float(expr_tok[0])
+        i = 1
+        try:
+            while i < len(expr_tok):
+                operator = expr_tok[i]
+                next_num = float(expr_tok[i+1])
+                if operator == "+":
+                    result += next_num
+                elif operator == "-":
+                    result -= next_num
+                i+=2
+        except Exception as e:
+            print("Error: ", e)
+        print(expr_tok)
+        print(result)
+        self.input.delete(0,tk.END)
+        self.input.insert(tk.END, str(result))
+
+## Modified function to use regex
+    def calculate_expression_mod(self):
+        
+        text = self.input.get()
         cleaned_text = text.replace("x", "*")
+        regex_expr = ["/d+./d+d+[+\-*/]"]
         tokens = list(cleaned_text)
         i = 0
         if not tokens:
@@ -103,7 +148,6 @@ class Calculator:
         print(result)
         self.input.delete(0,tk.END)
         self.input.insert(tk.END, str(result))
-        
 
             
         pass
